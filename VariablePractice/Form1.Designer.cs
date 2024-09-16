31764d05e7fe87f51b4a4fd457970d40f0372688
lucamuel542 <lucamuel542@SDS-L-DLFDF63>
2024-09-16 11:20:42 -0400

    Finisheddd

--- VariablePractice/Form1.Designer.cs
+++ VariablePractice/Form1.Designer.cs
@@ -32,13 +32,16 @@
             this.hockeyButton = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
             this.payButton = new System.Windows.Forms.Button();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.carpetSaleButton = new System.Windows.Forms.Button();
+            this.billButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hockeyButton
             // 
             this.hockeyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hockeyButton.Location = new System.Drawing.Point(26, 18);
-            this.hockeyButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.hockeyButton.Margin = new System.Windows.Forms.Padding(2);
             this.hockeyButton.Name = "hockeyButton";
             this.hockeyButton.Size = new System.Drawing.Size(81, 33);
             this.hockeyButton.TabIndex = 1;
@@ -56,6 +59,7 @@
             this.outputLabel.Size = new System.Drawing.Size(287, 257);
             this.outputLabel.TabIndex = 4;
             this.outputLabel.Text = "...";
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
             // 
             // payButton
             // 
@@ -67,6 +71,40 @@
             this.payButton.TabIndex = 5;
             this.payButton.Text = "Pay";
             this.payButton.UseVisualStyleBackColor = true;
+            this.payButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.payButton_MouseClick);
+            // 
+            // circleButton
+            // 
+            this.circleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circleButton.Location = new System.Drawing.Point(26, 122);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(75, 39);
+            this.circleButton.TabIndex = 6;
+            this.circleButton.Text = "Circle";
+            this.circleButton.UseVisualStyleBackColor = true;
+            this.circleButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.circleButton_MouseClick);
+            // 
+            // carpetSaleButton
+            // 
+            this.carpetSaleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpetSaleButton.Location = new System.Drawing.Point(26, 182);
+            this.carpetSaleButton.Name = "carpetSaleButton";
+            this.carpetSaleButton.Size = new System.Drawing.Size(75, 37);
+            this.carpetSaleButton.TabIndex = 7;
+            this.carpetSaleButton.Text = "Carpet";
+            this.carpetSaleButton.UseVisualStyleBackColor = true;
+            this.carpetSaleButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // billButton
+            // 
+            this.billButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billButton.Location = new System.Drawing.Point(26, 225);
+            this.billButton.Name = "billButton";
+            this.billButton.Size = new System.Drawing.Size(75, 41);
+            this.billButton.TabIndex = 8;
+            this.billButton.Text = "Bill";
+            this.billButton.UseVisualStyleBackColor = true;
+            this.billButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.billButton_MouseClick);
             // 
             // Form1
             // 
@@ -74,11 +112,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(431, 302);
+            this.Controls.Add(this.billButton);
+            this.Controls.Add(this.carpetSaleButton);
+            this.Controls.Add(this.circleButton);
             this.Controls.Add(this.payButton);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.hockeyButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Variables Practice";
             this.ResumeLayout(false);
@@ -89,6 +130,9 @@
         private System.Windows.Forms.Button hockeyButton;
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.Button payButton;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.Button carpetSaleButton;
+        private System.Windows.Forms.Button billButton;
     }
 }
 
--- VariablePractice/Form1.cs
+++ VariablePractice/Form1.cs
@@ -10,17 +10,71 @@ using System.Windows.Forms;
 
 namespace VariablePractice
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            outputLabel.Text = "";
+
         }
 
         private void hockeyButton_Click(object sender, EventArgs e)
         {
-
+            String playerName = "Wanye Gretzky";
+            int playerNumber = 99;
+            outputLabel.Text += $"\n{playerName} is number {playerNumber}";
         }
 
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void payButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            double hours = 25;
+            double pay = 18.75;
+            double earned = hours * pay;
+            outputLabel.Text += $"\n{hours} hours at {pay} per hours equals {earned}";
+        }
+
+        private void circleButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            double radius = 15;
+            double pi = 3.14;
+            double area = radius * radius * pi;
+            outputLabel.Text += $"\nThe area of a circle with a radius of {radius}cm is {area}cm^2";
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            double length = 8.5;
+            double width = 6;
+            double costPerMetre = 19.95;
+            double areaCarpet = length * width;
+            double totalCost = length * width * costPerMetre;
+            outputLabel.Text += $"\nThe area of a room with dimensions {length}m x {width}m is {areaCarpet}m^2";
+            outputLabel.Text += $"\nThe cost to carpet this area is ${costPerMetre} per square metre is ${totalCost}";
+        }
+
+        private void billButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            double shirt = 12.49;
+            double money = 20.00;
+            double tax = 0.13;
+            double taxAmount = tax * shirt;
+            double totalCost = taxAmount + shirt;
+            double change = money - totalCost;
+
+           
+            outputLabel.Text += $"\nBill of Sale";
+            outputLabel.Text += $"\n\nShirt:          ${shirt}";
+            outputLabel.Text += $"\nTax:            ${taxAmount}";
+            outputLabel.Text += $"\nTotal:          ${totalCost}";
+            outputLabel.Text += $"\n\nTendered:       ${money}.00";
+            outputLabel.Text += $"\n\nChange:         ${change}";
+        }
     }
 }
